cb5214790f91e95fe9cc8a6cd38a5f8869be2f58
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-03-04 12:52:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# BMH MinION Basecaller Workflow
+
+Internal BMH/BFSSI script to basecall, demultiplex, then zip up a MinION run in preparation for upload to the 
+BMH Genomics Portal.
+
+Pipeline overview:
+1) Samplesheet validation
+2) Basecalling of FAST5 files generated with MinKNOW with [Guppy GPU](https://community.nanoporetech.com/protocols/Guppy-protocol/v/gpb_2003_v1_revo_14dec2018/linux-guppy)
+3) Demultiplexing with [qcat](https://github.com/nanoporetech/qcat)
+4) Compression with [7-Zip](https://www.7-zip.org/)
+
+### SampleSheet overview
+The samplesheet must be an **.xlsx** file and should look something like the following:
+
+| Sample_ID       | Sample_Name  | Barcode   | Run_ID           | Run_Protocol               | Instrument_ID | Sequencing_Kit | Flowcell_Type | Project_ID              | Read_Type | User        |
+|-----------------|--------------|-----------|------------------|----------------------------|---------------|----------------|---------------|-------------------------|-----------|-------------|
+| MIN-2019-000021 | 2015026      | barcode07 | 20191118_2053_1D | Rapid Barcoding Sequencing | MN26570       | SQL-RBK004     | FLO-MIN106    | Chicken_Nugget_Project  | 1D        | Dussault, F. |
+| MIN-2019-000022 | 2018001      | barcode08 | 20191118_2053_1D | Rapid Barcoding Sequencing | MN26570       | SQL-RBK004     | FLO-MIN106    | Chicken_Nugget_Project  | 1D        | Dussault, F. |
+| MIN-2019-000023 | D-Prime      | barcode09 | 20191118_2053_1D | Rapid Barcoding Sequencing | MN26570       | SQL-RBK004     | FLO-MIN106    | Chicken_Nugget_Project  | 1D        | Dussault, F. |
+| MIN-2019-000024 | 1185-Ampure  | barcode10 | 20191118_2053_1D | Rapid Barcoding Sequencing | MN26570       | SQL-RBK004     | FLO-MIN106    | Chicken_Nugget_Project  | 1D        | Dussault, F. |
+| MIN-2019-000025 | 1185 -Zymo   | barcode11 | 20191118_2053_1D | Rapid Barcoding Sequencing | MN26570       | SQL-RBK004     | FLO-MIN106    | Chicken_Nugget_Project  | 1D        | Dussault, F. |
+| MIN-2019-000026 | 1185-Spri    | barcode12 | 20191118_2053_1D | Rapid Barcoding Sequencing | MN26570       | SQL-RBK004     | FLO-MIN106    | Chicken_Nugget_Project  | 1D        | Dussault, F. |
+
+##### Important notes on SampleSheet:
+- All columns are required, must be populated, and must be named as shown.
+
+- Entries in the Sample_ID column must follow the `MIN-YYYY-XXXXXX` nomenclature.
+
+- Entries in the Project_ID column **cannot contain spaces or special characters**.
+
+- Upon completion of the pipeline, the input samplesheet will be renamed **SampleSheet.xlsx**. 
+This is required for upload to the portal.
+
+### Installation instructions
+
+```
+git clone {todo}
+cd {todo}
+conda create -f environment.yml
+conda activate envname
+```
---
+++ minion_basecaller.py
@@ -0,0 +1,172 @@
+import click
+import shutil
+from pathlib import Path
+from subprocess import Popen, PIPE
+import pandas as pd
+import re
+
+"""
+TODO:
+Automatically parse out the flowcell and kit. Kit will be in the samplesheet and
+flowcell is in final_summary.txt
+"""
+
+
+def validate_minion_project_id(value: str):
+    if ' ' in value:
+        print(f"ERROR: Found whitespace character in Project_ID column with value '{value}'")
+        return False
+    return True
+
+
+def validate_minion_sample_id(value: str, length: int = 15):
+    """
+      Strict validation of BMH MinION Sample ID
+      :param value: sample_id
+      :param length: expected length of string
+      """
+    components = value.split("-")
+
+    if len(value) != length:
+        print(f"MinION Sample ID '{value}' does not meet the expected length of 15 characters. "
+              f"MinION Sample ID must be in the following format: 'MIN-2020-000001'")
+    if len(components) != 3:
+        print(f"MinION Sample ID '{value}' does not appear to meet expected format. "
+              f"MinION Sample ID must be in the following format: 'BMH-2018-000001'")
+    elif components[0] != 'MIN':
+        print(
+            f"TEXT component of MinION Sample ID ('{value}') does not equal expected 'MIN'")
+    elif not components[1].isdigit() or len(components[1]) != 4:
+        print(
+            f"YEAR component of MinION Sample ID ('{value}') does not equal expected 'YYYY' format")
+    elif not components[2].isdigit() or len(components[2]) != 6:
+        print(
+            f"ID component of MinION Sample ID ('{value}') does not equal expected 'XXXXXX' format")
+    else:
+        return True
+    return False
+
+
+def run_subprocess(cmd):
+    p = Popen(cmd, shell=True)
+    p.wait()
+
+
+def validate_samplesheet(samplesheet: Path):
+    expected_columns = [
+        'Sample_ID',
+        'Sample_Name',
+        'Barcode',
+        'Run_ID',
+        'Run_Protocol',
+        'Instrument_ID',
+        'Sequencing_Kit',
+        'Flowcell_Type',
+        'Project_ID',
+        'Read_Type',
+        'User'
+    ]
+
+    samplesheet_valid = True
+    df = pd.read_excel(samplesheet, index=None)
+
+    while samplesheet_valid:
+        for col in expected_columns:
+            if col not in list(df.columns):
+                print(f"ERROR: Could not find expected column '{col}' in input samplesheet!")
+                samplesheet_valid = False
+
+        for key, val in df.iterrows():
+            samplesheet_valid = validate_minion_sample_id(val['Sample_ID'])
+            samplesheet_valid = validate_minion_project_id(val['Project_ID'])
+
+        break
+
+    if not samplesheet_valid:
+        print('ERROR: Quitting program due to validation error(s) in SampleSheet.')
+        quit()
+
+
+def call_guppy(fast5_dir, output_dir, flowcell, kit):
+    output_dir = output_dir / 'guppy_basecalling'
+    output_dir.mkdir(exist_ok=True, parents=True)
+    print(f"Running guppy on {fast5_dir} and storing output in {output_dir}")
+    print(f"Flowcell: {flowcell}")
+    print(f"Kit: {kit}")
+    cmd = f"guppy_basecaller -i {fast5_dir} -s {output_dir} --device cuda:0 --flowcell {flowcell} --kit {kit} --trim_barcodes --recursive --chunk_size 1700 --gpu_runners_per_device 4"
+    print(cmd)
+    run_subprocess(cmd)
+    return output_dir
+
+
+def call_cat(fastq_dir, output_dir):
+    """
+    Concatenates input FASTQ files into a single FASTQ. Output from this command is fed to qcat for demultiplexing.
+    """
+    print(f"Detected {len(list(fastq_dir.glob('*.fastq')))} FASTQ files in {fastq_dir}")
+    outfile = output_dir / (output_dir.name + '_combined.fastq')
+    print(f"Concatenating all FASTQ files in {fastq_dir} and storing contents in {outfile}")
+    cmd = f"cat {fastq_dir}/*.fastq > {outfile}"
+    print(cmd)
+    run_subprocess(cmd)
+    return outfile
+
+
+def call_qcat(fastq, output_dir):
+    """
+    Demultiplexes FASTQ file according to automatically detected barcodes. Trims the barcodes.
+    """
+    output_dir = output_dir / 'qcat_demultiplexing'
+    output_dir.mkdir(exist_ok=True, parents=True)
+    print(f"Demultiplexing {fastq.name} with qcat and storing output in {output_dir}")
+    cmd = f"qcat -f {fastq} -b {output_dir} --trim --tsv --kit Auto > {output_dir}/qcat_log.txt"
+    print(cmd)
+    run_subprocess(cmd)
+    return output_dir
+
+
+def call_pigz(fastq_dir):
+    """
+    Gzips all fastq files in a given directory. Used to gzip the contents of the qcat demultiplexed output folder.
+    """
+    print(f"Compressing all FASTQ files in {fastq_dir}")
+    cmd = f"gzip {fastq_dir}/*.fastq"
+    print(cmd)
+    run_subprocess(cmd)
+    return fastq_dir
+
+
+def pipeline(input_dir: Path, output_dir: Path, samplesheet: Path, flowcell: str, kit: str):
+    validate_samplesheet(samplesheet=samplesheet)
+    shutil.copy(str(samplesheet), str(output_dir / 'SampleSheet.xlsx'))
+    fastq_dir = call_guppy(input_dir, output_dir, flowcell, kit)
+    combined_fastq = call_cat(fastq_dir, output_dir)
+    demultiplex_dir = call_qcat(combined_fastq, output_dir)
+    call_pigz(demultiplex_dir)
+
+
+@click.command(
+    help="Wrapper script to conduct basecalling and demultiplexing on a MinION run.\nForest Dussault (forest.dussault@canada.ca). 2020/03/04.")
+@click.option('-i', '--input-dir', type=click.STRING,
+              help="Path to the MinION output folder containing FAST5 files (e.g. /var/lib/MinKNOW/data/20191118/MN26570/20191118_2053_1D/fast5).")
+@click.option('-o', '--output-dir', type=click.STRING,
+              help="Path to desired output directory for basecalled and demultiplexed files")
+@click.option('-s', '--samplesheet', type=click.STRING,
+              help="Path to samplesheet (.xlsx). Will automatically be copied to the output directory. Please refer to README.md for instructions on populating this file.")
+@click.option('-f', '--flowcell', type=click.STRING,
+              help="Flowcell type used for the MinION run. Defaults to FLO-MIN106.", default="FLO-MIN106")
+@click.option('-k', '--kit', type=click.STRING, help="Kit used for the MinION run. Defaults to SQK-RBK004.",
+              default="SQK-RBK004")
+def cli(input_dir, output_dir, samplesheet, flowcell, kit):
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True, parents=True)
+    samplesheet = Path(samplesheet)
+    print(f"Started Basecalling workflow")
+    pipeline(input_dir, output_dir, samplesheet, flowcell, kit)
+    print(f"Done! Output available in {output_dir}")
+
+
+if __name__ == "__main__":
+    validate_samplesheet(Path('/home/forest/PycharmProjects/BMH_MinION_Basecaller/SampleSheet_example.xlsx'))
+    # cli()
